860507b3820db96dbfd3dc82bd439119bfe77603
Señor Snail <loehler@uni-osnabrueck.de>
2021-02-25 19:13:22 +0100

    Stand 25.02 parsing somewhat works

--- README.md
+++ README.md
@@ -4,30 +4,30 @@ Programming Task Methods of AI Seminar
 This is the Project for the Methods of AI Seminar in which we were tasked to program a Planner capable of solving problems in classical PDDL-syntax.
 
 ## expressions.py
-A file which provides functionality to make worlds and expressions from syntax trees (created in pddl.py)
+A file which provides functionality to make worlds and expressions from syntax trees (created in pddl.py) \n
   While the code might be not overly object oriented (for new operators no new class has to be implemented but the behavior of the functions and in ```Expression()``` ```apply_self_on()```, one can still consider it elegant code. ;)
  
 ## graph.py
 A file defining a "lazy" representation of an graph (by only representing the neighbors of each node)
-  This file has already been provided
+  \n\tThis file has already been provided
   
 ## pathfinding.py
 A file that implements A* which traverses the graph provided by Graph.py
-  A* seems to could perform better (based on Terminal output of Teacher), meaning less nodes extensions and visits
-	Possible explanation: (visited is supposed to only count the first time when node is explored (not when its reevaluated))
-  Unrelated: if a path with lower cost of an existing node is found, this node is added to frontier *again* instead of being replaced
+  \n\tA* seems to could perform better (based on Terminal output of Teacher), meaning less nodes extensions and visits
+	\n\tPossible explanation: (visited is supposed to only count the first time when node is explored (not when its reevaluated))
+  \n\tUnrelated: if a path with lower cost of an existing node is found, this node is added to frontier *again* instead of being replaced
   
 ## .pddl
 Files expressing a problem domain and the problem itself (wumpus problem) which serves testing purposes
 
 ## pddl.py
 A file that parses pddl domains and problems to be used in planner
-  IN DEVELOPEMENT
+  \n\tIN DEVELOPEMENT
   
 ## planner.py
 A file that uses A* implemented in pathfinding on the expressions created by pddl.py and expressions.py
-  IN DEVELOPEMENT
+  \n\tIN DEVELOPEMENT
 
 ## runall.py
 Given an directory with at least pddl domain and problem it will run all pddl problems (with heuristic on)
-  This file has already been provided
+  \n\tThis file has already been provided
--- pddl.py
+++ pddl.py
@@ -1,6 +1,7 @@
 import sys
 import expressions
-import tokenize
+import re
+from queue import LifoQueue as LQ
 
 SUPPORTED_REQUIREMENTS = [":strips",":typing",":disjunctive-preconditions",":equality",":existential-preconditions",":universal-preconditions",":conditional-effects",":adl"]
 
@@ -12,17 +13,28 @@ class NotSupported(Exception):
         
 def parser(fname):
     stack = []
+    str_file = ""
     file = open(fname, "r")
-    tockens = tokenize(file) # TODO: Ignore PDDL Comments starting with ";"
-    for tocken in tockens:
-        stack.push(tocken)
-        if tocken == ")":
-            l = []
-            while not tocken == "(":
-                l.append(stack.pop)
-            stack.append(l)
+    for line in file.readlines():
+        if not ";" in line: # Comments are assumed to be on new lines (like it is in all the cases I looked so far)
+                            # Now it checks for all charackters in whole file (would be better if it checks just for the first one)
+            str_file += line.lower() # PDDL is case sensitive
     file.close()
-    return stack
+    tockens = re.findall(r"([:|\?]?[\w+-?]+\w+|[(|)|-])", str_file) # getts all the occurences of the stuff we need
+                        # translation of regular expression optionally ":" or "?" with arbitrarily many letters following it (word or number)
+                        #                                    optionally followed by other words joined by "-"
+                        #                                    or paranthesis or "-"
+    for tocken in tockens:
+        if tocken == ")": # Backtracing to last opening paranthesis from closing paranthesis and adding it to the stack again (as a sublist)
+            l = [stack.pop()]
+            while not l[-1] == "(":
+                l.append(stack.pop())
+            l.pop() # removing "("
+            stack.append(l[::-1])
+        else:
+            stack.append(tocken)
+    return stack[0] # stack has only one element
+
     
 def parse_domain(fname):
     """
@@ -30,21 +42,29 @@ def parse_domain(fname):
     
     The return value of this function is passed to planner.plan, and does not have to follow any particular format
     """
-    domain = parser(fname)[0] # ignore define
+    domain = parser(fname)
     
     #checking requirements
-    requirements = [] # TODO: get requirements from domain
+    requirements = domain[2][1:] # no check needed, requirements is not optional
     for x in requirements:
         if x not in SUPPORTED_REQUIREMENTS:
             raise NotSupported(x, "Planner does not support it")
     
+    type_to_constant = {key: None for key in domain[3][1:]} # should I check first if domain[3][0] == ":types"  ?
+    for i, constant in enumerate(domain[4][3::3]): # again, should I check first if domain[4][0] == ":constants"  ?
+                                                    # for every third element in the constants list of the domain (starting from 3rd)
+        type_to_constant[constant] = [domain[4][i*3+1]] # map the type to the constant as list (+1 to ignore "constants")
+        # mapping to sets of constants. Lists for the non constants (objects) are later provided by parse problem
+        
+    # ----------- works - so - far ---------------
+            
+    type_hierachy = {}
+            
     # Translating actions into when expressions (with every variable substitution from variable)?
-    # Creating and filling dom_dic NECESSARY?
-    domain_name = "" # TODO: get domain_name
-    dom_dic = {"domain":domain_name, "types":[], "constants":[], "predicates":[], "actions":[]} # WARNING predicates is optional
+    act_sch = [] # TODO fill action schemata
     
 # it is recommended to return a list of an action schemata representation, a dictionary mapping types to sets of constants for each type, and the type hierarchy information. Take care to include an extra mapping from the type "" to a set of all objects.
-    return None
+    return act_sch, type_to_constant, type_hierachy
     
 def parse_problem(fname):
     """
